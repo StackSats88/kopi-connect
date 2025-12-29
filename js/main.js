065fb84bbac895f45868c0ab63582bb7dc6a64f0
thinktinker <martinleongmo@gmail.com>
2025-12-29 13:21:01 +0800

    update keywords code from jeremy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// * Function for navbar to show and hide each sections
 setupNavigation();
 
 function setupNavigation() {
@@ -30,4 +31,94 @@ function setupNavigation() {
             this.classList.add('active');
         });
     });
+}
+
+
+// * Function to dynamically display keyword buttons in chatbot section
+const keywords = [
+    {
+        "id": 1,
+        "name": "Crisis",
+        "color": "#EF4444",
+    },
+    {
+        "id": 2,
+        "name": "Crisis",
+        "color": "#EF4444",
+    },
+    {
+        "id": 3,
+        "name": "General",
+        "color": "#198754",
+    },
+    {
+        "id": 4,
+        "name": "General",
+        "color": "#198754",
+    },
+    {
+        "id": 5,
+        "name": "Male",
+        "color": "#0d6efd",
+    },
+    {
+        "id": 6,
+        "name": "Male",
+        "color": "#0d6efd",
+    },
+    {
+        "id": 7,
+        "name": "Female",
+        "color": "#FF69B4",
+    },
+    {
+        "id": 8,
+        "name": "Female",
+        "color": "#FF69B4",
+    },
+];
+
+// Function to create keyword buttons dynamically
+function createChatButtons() {
+    const container = document.getElementById('chatButtonsContainer');
+
+    const buttonsPerRow = 4;
+    const numRows = keywords.length / buttonsPerRow;
+
+    for (let rows = 0; rows < numRows; rows++) {
+        const row = document.createElement('div');
+        row.className = 'button-row';
+        if (rows < numRows - 1) {
+            row.classList.add('mb-2');
+        }
+
+        const startIndex = rows * buttonsPerRow;
+        const endIndex = Math.min(startIndex + buttonsPerRow, keywords.length);
+
+        for (let buttons = startIndex; buttons < endIndex; buttons++) {
+            const button = document.createElement('button');
+            button.className = 'chat-button';
+            button.textContent = keywords[buttons].name;
+            button.style.background = keywords[buttons].color;
+            button.style.color = 'white';
+
+            button.addEventListener('click', function () {
+                handleButtonClick(keywords[buttons]);
+            });
+
+            row.appendChild(button);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createChatButtons();
+
+// TODO: Function to handle keyword button clicks and dynamically display the services in the chat
+function handleButtonClick(btnKeyword) {
+    // Shows what buttons are pressed
+    console.log(btnKeyword);
+
+    // Add your own functions here
 }
